019478f4cdb7cf41ade18e9fbda6c77e79331de2
AlienX420710 <47496458+AlienX420710@users.noreply.github.com>
2026-08-30 16:37:43 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = "Cleaning By Cassi";
 export const SITE_DESCRIPTION = "Done with precision. Peace of mind delivered.";
+export const BUSINESS_START_DATE = new Date('2025-07-03');
+export const OWNER_NAME = 'Cassi';
+export const SERVICE_AREA = 'Fox Cities & Surrounding Areas';
+export const CONTACT_EMAIL = 'cassandramorris@cleaningbycassi.com';
