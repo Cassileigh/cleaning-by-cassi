1999fb17e2940f0249e16f9ab2f05cdac8d76f39
Cassileigh <cassimorris23@gmail.com>
2026-09-14 16:36:38 -0500

    feat: add homepage service area map and contact section above footer

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,5 @@ export const OWNER_NAME = 'Cassi';
 export const SERVICE_AREA = 'Fox Cities & Surrounding Areas';
 export const CONTACT_EMAIL = 'cassandramorris@cleaningbycassi.com';
 export const FACEBOOK_URL = 'https://facebook.com/cleaningbycassi';
+export const CONTACT_PHONE = '(920) 215-1572';
+export const CONTACT_PHONE_HREF = 'tel:+19202151572';
